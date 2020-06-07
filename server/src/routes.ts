af5b0db9b210a6165420841dd15a2ac92a81115a
Nadoroski <30608671+Nadoroski@users.noreply.github.com>
2020-06-07 09:58:54 -0300

    Validação
    Validação de cadastro de novos pontos

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,6 +4,7 @@ import multer from 'multer';
 import multerConfig from './config/multer';
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
+import {celebrate, Joi} from 'celebrate'
 
 const routes = express.Router();
 const pointsController = new PointsController();
@@ -18,7 +19,25 @@ routes.get('/items', itemsController.index);
 routes.get('/points/:id', pointsController.show);
 routes.get('/points', pointsController.index);
 
-routes.post('/points', upload.single('image'), pointsController.create);
+routes.post(
+    '/points', 
+    upload.single('image'), 
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email:Joi.string().required().email(),
+            whatsapp: Joi.number().required(),
+            latitude:Joi.number().required(),
+            longitude:Joi.number().required(),
+            cidade: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            items:Joi.string().required(),
+
+        })
+    },{
+        abortEarly: false
+    }),
+    pointsController.create);
 
 
 
